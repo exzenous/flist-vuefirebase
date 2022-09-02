87c2ca2385d1b8a4184fe6a5f46fc293e488e81e
Nathachai <exzenous@outlook.com>
2022-09-02 12:54:21 +0700

    Add Event Detail on Modal

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
     import EventModule from './EventModule.vue'
-    import Modal from './components/Modal.vue'
+    import ModalAddEvent from './components/ModalAddEvent.vue'
 
     export default {
         data() {
@@ -33,16 +33,9 @@
               this.isAddingEvent = !this.isAddingEvent
             },
             addEvent(newTitle) {
-            
-            this.localStorage.push(
-                    {
-                        eventTitle: newTitle, 
-                        description: "A Mocked Description", 
-                        date: "A Mocked Description", 
-                        friendsList: []
-                    }
-                )
-            this.newEventTitle = ""
+                this.toggleModal()
+                this.localStorage.push(newTitle)
+                this.newEventTitle = ""
             },
             updateList(newList) {
                 this.localStorage = newList
@@ -50,19 +43,18 @@
         },
         components: {
             EventModule,
-            Modal
+            ModalAddEvent
         }
     }
 </script>
 
 <template>
-    <Modal v-if="isAddingEvent" />
+    <ModalAddEvent v-if="isAddingEvent" :eventProps="newEventTitle" @close="toggleModal" @updateList="addEvent"/>
     <div class="m-4 p-4 rounded bg-slate-200">
         <h1 class="p-4 text-3xl text-center">'Flist' - Friends List Management</h1>
         <div class="flex w-full">
             <input v-model="newEventTitle" class="flex-grow rounded p-2" type="text" placeholder="Event Name...">
-            <button class="ml-6 min-w-[15%] border-solid border-2 border-indigo-800 bg-indigo-500 text-white rounded" @click="addEvent(newEventTitle)">Add</button>
-            <button class="ml-6 min-w-[15%] border-solid border-2 border-indigo-800 bg-indigo-500 text-white rounded" @click="toggleModal">IF TEST</button>
+            <button class="ml-6 min-w-[15%] border-solid border-2 border-indigo-800 bg-indigo-500 text-white rounded" @click="toggleModal">Add</button>
         </div>
     </div>
     <!-- TODO: LET'S MAKE A POPUP MODAL FOR ADDITIONAL INFOS-->
---
+++ src/components/ModalAddEvent.vue
@@ -0,0 +1,58 @@
+<script>
+    export default {
+        emits: ['close', 'updateList'],
+        data() {
+            return {
+                newEventEntity: {
+                    eventTitle: this.eventProps,
+                    description: "",
+                    date: "",
+                    friendsList: []
+                }
+            }
+        },
+        props: {
+            'eventProps': String
+        },
+        methods: {
+            updateList() {
+                this.$emit('updateList', this.newEventEntity)
+            },
+            closeModal() {
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h2 class="text-3xl text-center">Adding: {{ eventProps }}</h2>
+            <div class="p-8 text-center">
+                <label for="description">Description</label><input v-model="newEventEntity.description" class="border" type="text"><br>
+                <label for="date">Date</label><input v-model="newEventEntity.date" class="border" type="text"><br>
+                <button class="my-4 ml-6 min-w-[15%] border-solid border-2 border-indigo-800 bg-indigo-500 text-white rounded" @click="this.updateList()">Add</button>
+                <button class="my-4 ml-6 min-w-[15%] border-solid border-2 border-indigo-800 bg-indigo-500 text-white rounded" @click="closeModal">Close</button>
+            </div>
+            <p>{{ newEventEntity }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+.modal {
+    width: 80%;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+</style>
